a3153d8260477833dcd657f37f0a298654ca9742
mi-rony <rony16006@gmail.com>
2026-04-04 21:00:41 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <ToastContainer></ToastContainer>
+    </>
+  );
+}
+
+export default App;
